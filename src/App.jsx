fac4f45292510e3acabcf21d22321739b987f6f1
wolfiee42 <saifalislam2022@gmail.com>
2023-09-15 11:35:22 +0600

    header section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './Components/Headear/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+    <Header></Header>
     </>
   )
 }
---
+++ src/Components/Headear/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h3 className="text-3xl font-bold text-center">Course Registration</h3>
+        </div>
+    );
+};
+
+export default Header;
